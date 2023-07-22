c92d02f6e0efb01ec1f1f9345e14b671bc298b6f
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-07-23 01:43:55 +0200

    Feat : ajout d'un premier style a la page de connexion

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -3,14 +3,21 @@ import '../styles/Connect.scss'
 const Connect = () => {
     return (
         <div className='central_connect_form'>
-            <h1>Mercure Connect</h1>
             <form>
+                <img src={process.env.PUBLIC_URL + './assets/Connect_Icon.png'} alt='' />
+                <h1>Mercure Login</h1>
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" />
-                <label htmlFor="password">Mot de passe</label>
+                <label htmlFor="password" >Mot de passe</label>
                 <input type="password" id="password" />
-                <button type="submit">Se connecter</button>
+                <span className='error'>* Mot de passe ou nom d'utilisateur incorrect</span>
+                <button type="submit" >Se connecter</button>
+                <div className='central_connect_form__links'>
+                    <a href="/register">Créer un compte</a>
+                    <a href="/forgot-password">Mot de passe oublié</a>
+                </div>
             </form>
+
         </div>
     );
 };
