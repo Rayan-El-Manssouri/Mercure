f508e0ccb1d2e839368e84c5a21d0c8880fdc3e3
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-07-22 23:50:36 +0200

    Fix : réorganisation des fichiers de message

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import json
 from flask_cors import CORS
 from flask_socketio import SocketIO, send
@@ -7,28 +7,32 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
+
 def read_messages_from_file():
     try:
         with open("Message.txt", "r") as file:
-            return json.load(file)
+            return json.loads(file.read())
     except FileNotFoundError:
         return []
 
+
+
 def save_messages_to_file(messages):
     with open("Message.txt", "w") as file:
-        json.dump(messages, file)
+        json.dump(messages, file, indent=4)
 
 messages = read_messages_from_file()
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    return jsonify({"messages": messages})
+    return jsonify({"messages": messages}), 200, {'Content-Type': 'application/json', 'Indent': 4}
 
 @app.route('/new-messages', methods=['GET'])
 def get_new_messages():
     last_message_id = int(request.args.get('lastMessageId', 0))
     new_messages = [message for message in messages if message['id'] > last_message_id]
-    return jsonify({"messages": new_messages})
+    return jsonify({"messages": new_messages}), 200, {'Content-Type': 'application/json', 'Indent': 4}
 
 @app.route('/send-message', methods=['POST'])
 def send_message():
@@ -46,9 +50,9 @@ def send_message():
 
         socketio.emit('new_message', new_message)  # Supprime l'argument 'broadcast'
 
-        return jsonify({"message": new_message}), 201
+        return jsonify({"message": new_message}), 201, {'Content-Type': 'application/json', 'Indent': 4}
     else:
-        return jsonify({"error": "Invalid JSON data"}), 400
+        return jsonify({"error": "Invalid JSON data"}), 400, {'Content-Type': 'application/json', 'Indent': 4}
 
 if __name__ == '__main__':
     socketio.run(app, port=8000)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomeForm from './HomeForm';
+import HomeForm from '../components/HomeForm';
 
 const Home = () => {
     return (
