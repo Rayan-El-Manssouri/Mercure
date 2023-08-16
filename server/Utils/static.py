f222e747a797a190a5043946f943aca907bd1ad9
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-08-16 15:26:45 +0200

    Feat : amméloiration de la syntaxe json des routes

--- server/Utils/static.py
+++ server/Utils/static.py
@@ -2,12 +2,29 @@ import json
 
 def serve_static_file(route):
     with open("./server/Utils/files_patchs.json", "r") as json_file:
-        file_paths = json.load(json_file)
-    if route in file_paths:
-        file_info = file_paths[route]
-        emplacement_connect = file_info['path']
-        extension = file_info['extension']
-        with open(emplacement_connect, 'rb') as f:
-            data = f.read()    
-            content_type = extension
-        return data, 200, {'Content-Type': content_type}
+        data = json.load(json_file)
+
+    # Extract the base path and files information from the JSON
+    base_path = data.get("base_path", "")
+    files = data.get("files", {})
+
+    # Determine the file category and route within the category
+    file_category = None
+    file_route = None
+    for category, category_files in files.items():
+        if route in category_files:
+            file_category = category
+            file_route = route
+            break
+
+    if file_category and file_route:
+        file_info = files[file_category][file_route]
+        emplacement = base_path + file_info["path"]
+        content_type = file_info.get("mine-type", files[file_category]["_common"]["mine-type"])
+
+        with open(emplacement, 'rb') as f:
+            content = f.read()
+
+        return content, 200, {'Content-Type': content_type}
+    else:
+        return "File not found", 404, {'Content-Type': 'text/plain'}
