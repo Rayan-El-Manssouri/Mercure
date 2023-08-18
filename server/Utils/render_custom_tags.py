0688601f458733861325b88f6f9e02ff7e9fe7b0
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-08-18 11:54:12 +0200

    Feat : changement du nom de la balise Majax en import

--- server/Utils/render_custom_tags.py
+++ server/Utils/render_custom_tags.py
@@ -2,11 +2,12 @@ import re
 
 def render_custom_tags(content, base_path):
     def custom_tag_replacer(match):
-        src = match.group(1)
-        src = base_path + src.decode("utf-8")
+        src = match.group(1).decode("utf-8")
+        src = base_path + src.strip()
 
         with open(src, "rb") as file:
             return file.read()
 
-    content_rendered = re.sub(b'<Majax src="([^"]+)" />', custom_tag_replacer, content)
+    pattern = b'<import src="([^"]+)" />'
+    content_rendered = re.sub(pattern, custom_tag_replacer, content)
     return content_rendered
