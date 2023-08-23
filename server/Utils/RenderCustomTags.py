7c842c893aeeffbe44e50c1860079b11a1086caf
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-08-23 22:22:03 +0200

    Feat : déplacement de code dans une class

--- server/Utils/RenderCustomTags.py
+++ server/Utils/RenderCustomTags.py
@@ -25,7 +25,6 @@ class RenderCustomTags:
                     # Remplace la balise <import> par le contenu du fichier
                     import_tag.replace_with(BeautifulSoup(content_import, 'html.parser'))
 
-
     def add_active_class_to_links(soup: BeautifulSoup, route: str) -> None:
         """Ajouter la classe 'active' aux liens <a> avec l'attribut 'exact' correspondant à 'route'."""
         target_links = soup.find_all('a', attrs={'exact': True})
@@ -33,3 +32,23 @@ class RenderCustomTags:
             if link.get('href') == route:
                 link['class'] = 'active'
             del link['exact']
+
+    def process_imports(soup: BeautifulSoup, base_path: str):
+        """Traite les balises <import> dans le contenu HTML."""
+        import_tags = soup.find_all('import')
+        import_list = []
+
+        for import_tag in import_tags:
+            src = import_tag.get('src')
+            if src:
+                with open(base_path + src, "r", encoding='utf-8') as file:
+                    content_import = file.read()
+                    import_list.append((import_tag, content_import))
+
+        # Remplacer les balises <import> par le contenu des fichiers importés
+        for import_tag, content_import in import_list:
+            import_tag.replace_with(BeautifulSoup(content_import, 'html.parser'))
+
+        # Vérifier si le contenu importé contient des balises <import> imbriquées
+        while soup.find('import'):
+            RenderCustomTags.replace_import_tags(soup, base_path) 
--- server/Utils/render_custom_tags.py
+++ server/Utils/render_custom_tags.py
@@ -1,33 +1,14 @@
 from bs4 import BeautifulSoup
 from Utils.RenderCustomTags import RenderCustomTags
+
 def render_custom_tags(emplacement: str, content_type: str, base_path: str, route: str) -> str:
     """Rendre le contenu en fonction du type de contenu."""
     content = RenderCustomTags.read_file(emplacement, content_type)
     
     if content_type == "text/html":
         soup = BeautifulSoup(content, 'html.parser')
-        process_imports(soup, base_path)
+        RenderCustomTags.process_imports(soup, base_path)
         RenderCustomTags.add_active_class_to_links(soup, route)
         return soup.prettify()
     
     return content
-
-def process_imports(soup: BeautifulSoup, base_path: str):
-    """Traite les balises <import> dans le contenu HTML."""
-    import_tags = soup.find_all('import')
-    import_list = []
-
-    for import_tag in import_tags:
-        src = import_tag.get('src')
-        if src:
-            with open(base_path + src, "r", encoding='utf-8') as file:
-                content_import = file.read()
-                import_list.append((import_tag, content_import))
-
-    # Remplacer les balises <import> par le contenu des fichiers importés
-    for import_tag, content_import in import_list:
-        import_tag.replace_with(BeautifulSoup(content_import, 'html.parser'))
-
-    # Vérifier si le contenu importé contient des balises <import> imbriquées
-    while soup.find('import'):
-        process_imports(soup, base_path)
