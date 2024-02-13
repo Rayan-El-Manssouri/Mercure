23882e6d35e52c5a3cd8185f885380ced9c537f3
Rayan-El-Manssouri <prorayanelmanssouri@gmail.com>
2024-02-13 10:42:15 +0100

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Mercure</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
